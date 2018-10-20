150838c627f71cd1599ff6fd7eee4093803ee05b
dfsnow <dfsnow@uchicago.edu>
2018-10-20 23:42:42 +0000

    Added input file scripts

---
+++ otp/scripts/create_dest_files.R
@@ -0,0 +1,14 @@
+library(gtfsr)
+library(tidyverse)
+
+cta <- import_gtfs("otp/graphs/17031/17031_cta.zip", local = TRUE)
+pace <- import_gtfs("otp/graphs/17031/17031_pace.zip", local = TRUE)
+
+cta_cleaned <- cta$stops_df %>%
+  select(stop_id, stop_lon, stop_lat) %>%
+  rename(
+    GEOID = stop_id,
+    Y = stop_lon,
+    X = stop_lat
+    )
+  
