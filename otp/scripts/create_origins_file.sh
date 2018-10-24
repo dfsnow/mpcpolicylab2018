60d97807bba29dde7d50bc714e86f57989b1106b
Dan Snow <dfsnow@uchicago.edu>
2018-10-24 00:31:04 -0500

    Created multithreaded OTP setup

--- otp/scripts/create_origins_file.sh
+++ otp/scripts/create_origins_file.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+GEOID=17031
+GEOMETRY='blocks'
+CHUNKS=40
+
 psql -d network -U snow -c "\COPY (
     SELECT geoid, ST_X(centroid) AS X, ST_Y(centroid) AS Y
-    FROM tracts
+    FROM $GEOMETRY
     WHERE ST_Contains((
         SELECT geom_buffer
         FROM counties
@@ -13,4 +17,16 @@ psql -d network -U snow -c "\COPY (
 echo "GEOID,Y,X" > $GEOID-origins.csv
 awk -F, '{ print $1,$3,$2 }' OFS=, temp.csv >> $GEOID-origins.csv
 
+if [ -n "$CHUNKS" ]; then
+    tail -n +2 $GEOID-origins.csv > temp.csv
+    rows=$(cat temp.csv | wc -l)
+    chunk_size=$(expr $rows / $CHUNKS)
+    split -l $chunk_size -d temp.csv x
+    for chunk in $(seq -f "%02g" 1 $CHUNKS); do
+        echo "GEOID,Y,X" > $GEOID-origins-$chunk.csv
+        cat x$chunk >> $GEOID-origins-$chunk.csv
+    done
+fi
+
+rm x*
 rm temp.csv
--- otp/scripts/submit_jobs_local.sh
+++ otp/scripts/submit_jobs_local.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 WORKING_DIR='/resources/'
+LOCATION_DIR='locations/'
 
 for x in 17031; do
 
     docker run -it --rm \
         -v /home/snow/mpcpolicylab2018/otp/:$WORKING_DIR \
         -e WORKING_DIR=$WORKING_DIR \
+        -e LOCATION_DIR=$LOCATION_DIR \
+        -e MAX_THREADS=40 \
+        -e CHUNKS=40 \
         -e GEOID=$x \
         otp
 
