b98f152a7127568dce56182590cf8b165b7e1bb2
dfsnow <dfsnow@uchicago.edu>
2018-11-15 00:20:03 +0000

    Fixed dest script

--- otp/scripts/create_destinations_file.R
+++ otp/scripts/create_destinations_file.R
@@ -9,24 +9,24 @@ cta_cleaned <- cta$stops_df %>%
   select(stop_id, stop_lon, stop_lat) %>%
   rename(
     GEOID = stop_id,
-    Y = stop_lon,
-    X = stop_lat
+    Y = stop_lat,
+    X = stop_lon
     )
 
 pace_cleaned <- pace$stops_df %>%
   select(stop_id, stop_lon, stop_lat) %>%
   rename(
     GEOID = stop_id,
-    Y = stop_lon,
-    X = stop_lat
+    Y = stop_lat,
+    X = stop_lon
   )
 
 metra_cleaned <- metra$stops_df %>%
   select(stop_id, stop_lon, stop_lat) %>%
   rename(
     GEOID = stop_id,
-    Y = stop_lon,
-    X = stop_lat
+    Y = stop_lat,
+    X = stop_lon
   )
 
 merged_stops <- bind_rows(cta_cleaned, pace_cleaned, metra_cleaned)
