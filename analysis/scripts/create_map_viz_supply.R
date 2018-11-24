863d8fc5de9c93cc877065596cb9ad7c7ae0bd08
dfsnow <dfsnow@uchicago.edu>
2018-11-24 15:40:44 +0000

    Updated scripts for paratransit and dial a ride data

---
+++ analysis/scripts/create_map_viz_supply.R
@@ -0,0 +1,183 @@
+library(leaflet)
+library(tidyverse)
+library(sf)
+library(snakecase)
+library(htmlwidgets)
+
+# List of the relevant counties
+il_counties <- c(
+  "031" = "Cook",
+  "043" = "DuPage",
+  "089" = "Kane",
+  "093" = "Kendall",
+  "097" = "Lake",
+  "111" = "McHenry",
+  "197" = "Will"
+)
+
+# Load tract data for IL, IN, and WI from census
+il_tracts <- st_read("analysis/shapefiles/gz_2010_17_140_00_500k.shp") %>%
+  setNames(tolower(names(.))) %>%
+  filter(county %in% names(il_counties)) %>%
+  mutate(geoid = paste0(state, county, tract)) %>%
+  select(-geo_id) %>%
+  st_transform(crs = 4326)
+
+wi_tracts <- st_read("analysis/shapefiles/gz_2010_55_140_00_500k.shp") %>%
+  setNames(tolower(names(.))) %>%
+  filter(county == "059") %>%
+  mutate(geoid = paste0(state, county, tract)) %>%
+  select(-geo_id) %>%
+  st_transform(crs = 4326)
+
+in_tracts <- st_read("analysis/shapefiles/gz_2010_18_140_00_500k.shp") %>%
+  setNames(tolower(names(.))) %>%
+  filter(county == "089") %>%
+  mutate(geoid = paste0(state, county, tract)) %>%
+  select(-geo_id) %>%
+  st_transform(crs = 4326)
+
+# Combine into one dataframe
+mpc_tracts <- il_tracts %>%
+  bind_rows(in_tracts) %>% 
+  bind_rows(wi_tracts) %>%
+  mutate(geometry = st_as_sfc(geometry))
+
+rm(list = c("il_tracts", "in_tracts", "wi_tracts"))
+
+# Load supply files
+supply <- read_csv("analysis/data/17031_output_blocks_index.csv") %>%
+  mutate(geoid = str_pad(origin, 11, "left", "0")) %>%
+  select(-origin)
+
+mpc_merged <- mpc_tracts %>%
+  left_join(supply, by = "geoid") 
+
+### Creating map helpers and map ###
+mpc_palette <- "viridis"
+
+mpc_labels <- sprintf(
+  paste0(
+    "<strong>%s</strong><br/>",
+    "Index: %g<br/>",
+    "<strong>Fixed Line Values</strong><br/>",
+    "Stop Count: %g<br/>",
+    "Stop Frequency: %g<br/>",
+    "Stop Coverage: %g<br/>",
+    "Walk Distance: %g<br/>",
+    "Connectivity: %g<br/>",
+    "<strong>Paratransit Values</strong><br/>",
+    "Paratransit Eligible: %g<br/>"),
+  mpc_merged$geoid,
+  round(mpc_merged$all_index, 3),
+  round(mpc_merged$f_count, 3),
+  round(mpc_merged$f_stop_freq, 3),
+  round(mpc_merged$f_stop_coverage, 3),
+  round(mpc_merged$f_walk_dist, 3),
+  round(mpc_merged$f_connectivity, 3),
+  round(mpc_merged$p_elig, 3)
+  ) %>%
+  lapply(htmltools::HTML)
+
+mpc_highlights <- highlightOptions(
+  weight = 4.5,
+  color = "#ffffff",
+  fillOpacity = 1,
+  bringToFront = TRUE)
+
+mpc_labeloptions <- labelOptions(
+  style = list("font-weight" = "normal", padding = "3px 8px"),
+  textsize = "15px",
+  direction = "auto")
+
+# Supply map helpers
+mpc_map_supply_pal_all <- colorQuantile(
+  palette = mpc_palette,
+  domain = mpc_merged$all_index,
+  n = 10)
+
+mpc_map_supply_pal_fixed <- colorQuantile(
+  palette = mpc_palette,
+  domain = mpc_merged$f_index,
+  n = 10)
+
+mpc_map_supply_pal_para <- colorNumeric(
+  palette = mpc_palette,
+  domain = mpc_merged$p_index)
+
+# Create leaflet maps
+mpc_map <- leaflet() %>%
+  addProviderTiles(providers$CartoDB.Positron) %>%
+  addPolygons(
+    data = st_geometry(mpc_merged),
+    fillColor = mpc_map_supply_pal_all(mpc_merged$all_index),
+    color = "#e2e2e2",
+    weight = 0.3,
+    smoothFactor = 0.2,
+    fillOpacity = 0.75,
+    label = mpc_labels,
+    labelOptions = mpc_labeloptions,
+    highlightOptions = mpc_highlights,
+    group = "Both"
+  ) %>%
+  addLegend(
+    title = "Deciles of <br>Overall Supply Index",
+    pal = mpc_map_supply_pal_all,
+    values = mpc_merged$all_index,
+    position = "topright",
+    group = "Both",
+    labFormat = function(type, cuts, p) {
+      n = length(cuts)
+      paste0(round(cuts[-n], 2), sep = " to ", round(cuts[-1], 2))}
+  ) %>%
+  addPolygons(
+    data = st_geometry(mpc_merged),
+    fillColor = mpc_map_supply_pal_fixed(mpc_merged$f_index),
+    color = "#e2e2e2",
+    weight = 0.3,
+    smoothFactor = 0.2,
+    fillOpacity = 0.75,
+    label = mpc_labels,
+    labelOptions = mpc_labeloptions,
+    highlightOptions = mpc_highlights,
+    group = "Fixed Line"
+  ) %>%
+  addLegend(
+    title = "Deciles of <br>Fixed Line Index",
+    pal = mpc_map_supply_pal_fixed,
+    values = mpc_merged$f_index,
+    position = "topright",
+    group = "Fixed Line",
+    labFormat = function(type, cuts, p) {
+      n = length(cuts)
+      paste0(round(cuts[-n], 2), sep = " to ", round(cuts[-1], 2))}
+  ) %>%
+  addPolygons(
+    data = st_geometry(mpc_merged),
+    fillColor = mpc_map_supply_pal_para(mpc_merged$p_index),
+    color = "#e2e2e2",
+    weight = 0.3,
+    smoothFactor = 0.2,
+    fillOpacity = 0.75,
+    label = mpc_labels,
+    labelOptions = mpc_labeloptions,
+    highlightOptions = mpc_highlights,
+    group = "Paratransit"
+  ) %>%
+  addLegend(
+    title = "Paratransit <br/>Eligible",
+    pal = mpc_map_supply_pal_para,
+    values = mpc_merged$p_index,
+    position = "topright",
+    group = "Paratransit",
+    labFormat = function(type, cuts, p) {
+      n = length(cuts)
+      paste0(round(cuts[-n], 2), sep = " to ", round(cuts[-1], 2))}
+  ) %>%
+  addLayersControl(
+    overlayGroups = c("Both", "Fixed Line", "Paratransit"),
+    position = "bottomright"
+  ) %>%
+  hideGroup(c("Fixed Line", "Paratransit")) 
+
+saveWidget(mpc_map, "mpc_map_20181121.html")
--- analysis/scripts/reduce_od_matrix_blocks.R
+++ analysis/scripts/reduce_od_matrix_blocks.R
@@ -2,27 +2,27 @@ library(tidyverse)
 library(udunits2)
 library(sf)
 
-##### ODM and GTFS #####
+##### Fixed Line (count, freq, coverage, dist) #####
 # Reading in the raw distance matrix for walking
 odm_walking <- read_csv("analysis/data/17031_output_blocks_walking.csv") %>% 
   mutate(walk_dist = ud.convert(walk_dist, "m", "mi"),
          origin = str_pad(origin, 15, "left", "0")
          )
 
-# Loading both GTFS feeds
+# Loading all GTFS feed stop data
 gtfs <- bind_rows(
   read_csv("analysis/data/cta_gtfs_stop_summary.csv") %>% mutate(stop_id = as.character(stop_id)),
   read_csv("analysis/data/pace_gtfs_stop_summary.csv"),
   read_csv("analysis/data/metra_gtfs_stop_summary.csv")
 )
 
-# Merging data about each stop to the OD matrix
+# Merging data about each stop to the OD matrix by destination
 odm_merged <- odm_walking %>%
   filter(walk_dist <= 0.75) %>%
   left_join(gtfs, by = c("destination" = "stop_id")) %>%
   mutate(accessible = wheelchair_boarding != 2)
 
-# Aggregating by each origin (block)
+# Aggregating stop information by each origin (block)
 odm_block_agg <- odm_merged %>%
   mutate_at(
     vars(agg_cost, walk_dist, stop_week_frequency, stop_week_pct_coverage),
@@ -44,43 +44,110 @@ odm_block_agg <- odm_merged %>%
     count = scales::rescale(count, to = c(0, 1)),
     tract_id = str_sub(origin, 1, 11)
   )
+
+rm(list = c("gtfs", "odm_walking"))
   
 
-##### ODM Transit Scores (Connectivity) #####
+
+##### Fixed Line Transit Scores (connectivity) #####
 # Reading in the matrix for transit + walking
 odm_transit <- read_csv("analysis/data/17031_output_blocks_transit.csv")
 
-# Get all stops within 30 min then sum their distances and rescale
+# Get count of all stops within 30 min and rescale
 odm_block_scores <- odm_transit %>%
-  filter(agg_cost <= 30) %>%
+  filter(agg_cost <= 60) %>%
   group_by(origin) %>%
   summarize(connectivity = n()) %>%
   mutate(connectivity = scales::rescale(connectivity, to = c(0, 1))) %>%
   mutate(origin = as.character(origin))
 
-# Merge tract pop and connectivity, then aggregate to tract level 
-odm_tract_agg <- odm_block_agg %>%
-  left_join(
-    read_csv("analysis/data/chi_block_data.csv") %>%
+# Merge block population and connectivity scores
+odm_block_agg <- odm_block_agg %>%
+  full_join(
+    read_csv("analysis/data/mpc_block_data.csv") %>%
       rename(origin = geoid) %>%
       mutate(origin = str_pad(origin, 15, "left", "0")
       ),
     by = "origin"
   ) %>%
-  mutate(block_pop = replace_na(block_pop, 0)) %>%
   left_join(odm_block_scores, by = "origin") %>%
+  mutate_all(funs(replace_na(., 0))) %>%
+  st_as_sf(coords = c("lon", "lat"), crs = 4326)
+
+rm(list = c("odm_transit"))
+
+
+
+##### Paratransit Coverage #####
+# Load paratransit coverage map (cta and pace)
+paratransit <- st_read("analysis/shapefiles/fixed_lines_buffered_merged.shp") %>%
+    st_set_crs(4326)
+  
+# Create indicator if a block is in paratransit coverage area
+odm_block_agg <- odm_block_agg %>%
+  mutate(p_elig = st_within(odm_block_agg, paratransit) %>% lengths > 0)
+
+#odm_block_agg %>% write_csv("analysis/data/block_agg_temp.csv")
+#odm_block_agg <- read_csv("analysis/data/block_agg_temp.csv") %>%
+#mutate(geometry = st_geometry(geometry))
+
+
+
+##### Dial-a-ride Coverage #####
+# Load dial-a-ride service data
+dar_data <- read_csv("analysis/data/dar_service_data.csv") %>%
+  filter(!is.na(objectid))
+
+# Load dial-a-ride shapefile
+dar_shapes <- st_read("analysis/shapefiles/GIS.DIAL_A_RIDE_2018.shp") %>%
+  setNames(tolower(colnames(.))) %>%
+  select(objectid, objectid_1)
+
+# Merge shapes and service data, keep only relevant data and prefix with d_
+dar_merged <- left_join(dar_shapes, dar_data, by = c("objectid", "objectid_1")) %>%
+  select(total_service_hours_pct_coverage, eligibility, advance_flexibility, geometry) %>%
+  setNames(c(paste0("d_", colnames(.)[-length(colnames(.))]), "geometry")) %>%
+  st_transform(crs = 4326)
+
+rm(list = c("dar_data", "dar_shapes"))
+
+# Perform a spatial join of DaR data to all blocks, then drop geometry
+odm_block_agg_dar <- odm_block_agg %>%
+  st_join(dar_merged, join = st_within)
+odm_block_agg_dar$geometry <- NULL
+
+# Get the count + mean eligibility, advanced call in, and pct_coverage per block
+odm_block_agg <- odm_block_agg_dar %>%
+  group_by(origin) %>%
+  summarize(d_count = n()) %>%
+  left_join(odm_block_agg_dar, by = "origin") %>%
+  mutate(tract_id = as.numeric(tract_id)) %>%
+  group_by(origin) %>%
+  summarize_all(funs(mean(.))) %>%
+  mutate(tract_id = str_pad(tract_id, 11, "left", "0")) %>%
+  mutate_at(vars(starts_with("d_")), funs(scales::rescale(., to = c(0, 1))))
+
+#rm(list = c("odm_block_agg_dar"))
+  
+
+##### Tract Aggregation #####
+odm_tract_agg <- odm_block_agg %>%
   group_by(tract_id) %>%
-  summarize(
-    count = weighted.mean(count, block_pop),
-    walk_dist = weighted.mean(walk_dist, block_pop),
-    stop_freq = weighted.mean(stop_freq, block_pop),
-    stop_coverage = weighted.mean(stop_coverage, block_pop),
-    connectivity = weighted.mean(connectivity, block_pop)
-    ) %>%
+  summarize_at(
+    vars(-block_pop, -tract_pop, -origin),
+    funs(weighted.mean(., block_pop)) 
+  ) %>%
+  mutate_all(funs(replace(., is.na(.), 0))) %>%
+  rename_at(
+    vars(count, walk_dist, stop_freq, stop_coverage, connectivity),
+    funs(paste0('f_', .))
+  ) %>%
   mutate(
-    index = count + walk_dist + stop_freq + stop_coverage + connectivity,
-    index = replace(index, is.nan(index), 0)
+    f_index = f_count + f_walk_dist + f_stop_freq + f_stop_coverage + f_connectivity,
+    p_index = p_elig + d_count + d_eligibility + d_advance_flexibility + d_total_service_hours_pct_coverage,
+    all_index = f_index + p_index
     ) %>%
+  filter(!is.nan(all_index)) %>%
   rename(origin = tract_id) %>%
   write_csv("analysis/data/17031_output_blocks_index.csv")
 
